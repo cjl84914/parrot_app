b51b7ca5f9579c0408768c5f4a4b533900a3ac5c
alexcai <alex@geetion.com>
2024-05-22 15:11:42 +0800

    修改readme

--- packages/maid_llm/src/llama_cpp/common/build-info.cpp
+++ packages/maid_llm/src/llama_cpp/common/build-info.cpp
@@ -1,4 +1,4 @@
-int LLAMA_BUILD_NUMBER = 0;
-char const *LLAMA_COMMIT = "unknown";
+int LLAMA_BUILD_NUMBER = 2;
+char const *LLAMA_COMMIT = "4198025";
 char const *LLAMA_COMPILER = "Android (7714059, based on r416183c1) clang version 12.0.8 (https://android.googlesource.com/toolchain/llvm-project c935d99d7cf2016289302412d708641d52d2f7ee)";
 char const *LLAMA_BUILD_TARGET = "x86_64-apple-darwin23.2.0";
